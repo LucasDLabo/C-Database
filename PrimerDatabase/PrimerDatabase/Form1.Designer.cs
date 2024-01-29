7ea65259458daeeb72cad811f8318da2d8e6816b
lucas del labo <lucas.dellabo@gmail.com>
2024-01-28 22:36:18 -0300

    Prueba de conexión a base de datos MySQL + Interfaz prototipo 1.0

--- PrimerDatabase/PrimerDatabase/Form1.Designer.cs
+++ PrimerDatabase/PrimerDatabase/Form1.Designer.cs
@@ -28,13 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPuerto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.txtBD = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor:";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(112, 23);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(100, 20);
+            this.txtServidor.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puerto:";
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.Location = new System.Drawing.Point(112, 69);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(100, 20);
+            this.txtPuerto.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Usuario:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Contraseña:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Base de datos a conectar:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(112, 123);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 7;
+            // 
+            // txtContra
+            // 
+            this.txtContra.Location = new System.Drawing.Point(131, 164);
+            this.txtContra.Name = "txtContra";
+            this.txtContra.Size = new System.Drawing.Size(100, 20);
+            this.txtContra.TabIndex = 8;
+            // 
+            // txtBD
+            // 
+            this.txtBD.Location = new System.Drawing.Point(199, 200);
+            this.txtBD.Name = "txtBD";
+            this.txtBD.Size = new System.Drawing.Size(100, 20);
+            this.txtBD.TabIndex = 9;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(131, 255);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 10;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(828, 450);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtBD);
+            this.Controls.Add(this.txtContra);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPuerto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServidor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MySQL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPuerto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContra;
+        private System.Windows.Forms.TextBox txtBD;
+        private System.Windows.Forms.Button btnConectar;
     }
 }
 
--- PrimerDatabase/PrimerDatabase/Form1.cs
+++ PrimerDatabase/PrimerDatabase/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,45 @@ namespace PrimerDatabase
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            String server = txtServidor.Text;
+            String puerto = txtPuerto.Text;
+            String user = txtUsuario.Text;
+            String password = txtContra.Text;
+            String database = txtBD.Text;
+
+            String conexion = "Database=" + database + "; Data Source=" + server + "; Port=" + puerto + "; User Id=" + user + "; Password=" + password;
+
+            MySqlConnection conexionbd = new MySqlConnection(conexion);
+            MySqlDataReader reader = null;
+            string data = null;
+
+            try
+            {
+                string consulta = "SHOW DATABASES";
+                MySqlCommand comando = new MySqlCommand(consulta);
+                comando.Connection = conexionbd;
+                conexionbd.Open();
+                reader = comando.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    data += reader.GetString(0) + "\n";
+                }
+
+                MessageBox.Show(data);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error:" + ex.Message);
+            }
+            finally 
+            {
+                conexionbd.Close();
+            }
         }
     }
 }
